7b7e590b4f6999c55206e441f1af93c7e5d53e6c
v43rus <leonard.jm@live.com>
2025-07-16 11:41:32 +0200

    Added cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import os
+from flask_cors import CORS
 from dotenv import load_dotenv
 
+frontend_url = os.getenv("FRONTEND_ORIGIN", "*")  # fallback till * om inte definierad
 
 def create_app():
     # Load environment variables from .env
@@ -11,5 +14,8 @@ def create_app():
     from .main import main as main_bp
 
     app.register_blueprint(main_bp)
+
+    # Enable CORS for all routes
+    CORS(app, resources={r"/api/*": {"origins": frontend_url}})
 
     return app
